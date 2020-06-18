08f1fbc49de8d33142cb3b8070c0d174aacdabf4
fabr1991 <56141382+fabr1991@users.noreply.github.com>
2020-06-17 23:34:40 -0400

    Update models.py

--- autofarmapage/models.py
+++ autofarmapage/models.py
@@ -1,10 +1,3 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from .managers import UserManager
@@ -158,7 +151,7 @@ class EntregaMedicamento(models.Model):
     codigo = models.ForeignKey(DetalleReceta, models.DO_NOTHING, db_column='codigo', related_name='+')
     id_receta = models.ForeignKey(DetalleReceta, models.DO_NOTHING, db_column='id_receta', related_name='+')
     rut_col_farmacia = models.ForeignKey(ColaboradorFarmacia, models.DO_NOTHING, db_column='rut_col_farmacia')
-    #id_tipo_entrega = models.ForeignKey(TipoEntrega, models.DO_NOTHING, db_column='id_tipo_entrega')
+    id_tipo_entrega = models.ForeignKey('TipoEntrega', models.DO_NOTHING, db_column='id_tipo_entrega')
 
     class Meta:
         managed = False
@@ -382,3 +375,7 @@ class Usuario(AbstractBaseUser):
     class Meta:
         managed = False
         db_table = 'usuario'
+
+
+
+        
