bc51d3114d5210faab6ae70070de98662ac61156
KarinaML <karinamorele@gmail.com>
2020-06-30 21:34:29 -0400

    It3

--- autofarma/settings.py
+++ autofarma/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'spgp9n@+!5$+no63hrfxkw2))t$xlgrgybm)rwo^qe45vv#&yw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.104', '127.0.0.1', '192.168.0.4']
+ALLOWED_HOSTS = ['192.168.43.104', '127.0.0.1', '192.168.0.4', '192.168.0.3']
 
 
 # Application definition
